c9375cb655fc82e3d9e1554220fe62733e442c93
brunatb <brutopanb@gmail.com>
2020-10-27 19:08:40 -0300

    Mudança para a página de posts

--- src/components/App.js
+++ src/components/App.js
@@ -13,15 +13,17 @@ import TimeLine from '../pages/TimeLine';
 export default function App(){
 
     return(
-        <Router>
-            <Switch>
-                <Route path='/timeline'>
-                    <TimeLine  />
-                </Route>
-                <Route path='/'>
-                    <Main  />
-                </Route>
-            </Switch>
-        </Router>
+        <UserProvider>
+            <Router>
+                <Switch>
+                    <Route path='/timeline'>
+                        <TimeLine  />
+                    </Route>
+                    <Route path='/'>
+                        <Main  />
+                    </Route>
+                </Switch>
+            </Router>
+        </UserProvider>
     );
 }
