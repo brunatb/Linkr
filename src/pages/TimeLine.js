dca55ad3c13c17b205c5c6a073b15c04057506a9
brunatb <brutopanb@gmail.com>
2020-10-27 17:46:41 -0300

    Guardar informação de cadastro do usuário

--- src/pages/TimeLine.js
+++ src/pages/TimeLine.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import UserContext from '../contexts/UserContext';
 
 export default function TimeLine(){
+    const {user} = useContext(UserContext);
     return(
         <h1>OI</h1>
     )
