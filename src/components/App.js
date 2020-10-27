017d03be4689d28ec0d5c0dd0a775f90787bc101
brunatb <brutopanb@gmail.com>
2020-10-27 14:34:00 -0300

    Criação do contexto do usuário

--- src/components/App.js
+++ src/components/App.js
@@ -5,21 +5,25 @@ import {
     Route
 } from "react-router-dom";
 
+import { UserProvider } from '../contexts/UserContext';
+
 import Main from '../pages/Main'
 import TimeLine from '../pages/TimeLine';
  
 export default function App(){
 
     return(
-        <Router>
-            <Switch>
-                <Route path='/timeline'>
-                    <TimeLine />
-                </Route>
-                <Route path='/'>
-                    <Main />
-                </Route>
-            </Switch>
-        </Router>
+        <UserProvider>
+            <Router>
+                <Switch>
+                    <Route path='/timeline'>
+                        <TimeLine />
+                    </Route>
+                    <Route path='/'>
+                        <Main />
+                    </Route>
+                </Switch>
+            </Router>
+        </UserProvider>
     )
 }
