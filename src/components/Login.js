dd777fec063a08e1c760e4bd5771ebeb1b14589c
analacaze <anaclaralacaze@gmail.com>
2020-10-27 17:21:01 -0300

    Desabilitar botão durante o request do login

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,15 +6,20 @@ import Forms from '../components/Forms';
 
 export default function Login({setTask}){
     const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
+    const [password, setPassword] = useState('');
+    const [enable,setEnable] = useState(false);
     const history = useHistory();
 
     function verifyInputs(){
         if (email === '' || password === '' )
             alert("Preencha todos os campos");
         else{
+            setEnable(true);
             const request = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v1/linkr/sign_in", {email, password});
-            request.then(() => history.push('/timeline')).catch(() => alert("Email/Senha incorretos"));
+            request.then(() => console.log("alou")).catch(() => {
+                alert("Email/Senha incorretos");
+                setEnable(false);
+            });
         }
     }
 
@@ -27,7 +32,7 @@ export default function Login({setTask}){
                 <input  type="password" name="senha" placeholder='password' 
                         onChange={e => setPassword(e.target.value)}
                         value={password}/>
-                <button onClick={verifyInputs}>Log In</button>
+                <button onClick={verifyInputs} type="submit" disabled={enable}>Log In</button>
             </Forms>
             <p onClick={() => setTask(false)}>First time? Create an account</p>
         </>
