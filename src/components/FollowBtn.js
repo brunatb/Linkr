1e60d1e1bf9e1609028d98e062a7ae7c9c519f99
brunatb <brutopanb@gmail.com>
2020-11-04 13:14:37 -0300

    Post de seguir e deixar de seguir usuário

--- src/components/FollowBtn.js
+++ src/components/FollowBtn.js
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import axios from 'axios';
 import styled from 'styled-components';
+import UserContext from '../contexts/UserContext';
+import { AiOutlineTrademark } from 'react-icons/ai';
 
 
-export default function FollowBtn(){
+export default function FollowBtn({id}){
+    const [text, setText] = useState('');
+    const{ userToken } = useContext(UserContext);
+    useEffect(() =>{
+        const request = axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v1/linkr/users/follows', userToken);
+        request.then(response =>{
+            verifyFollowers(response.data.users);
+        }).catch(() => alert('erro'))
+    }, [text])
+
+    function verifyFollowers(followers){
+        if(followers.length === 0){
+            setText('Follow');
+        }else{
+            let follower = followers.filter(f => f.id === id);
+            follower ? setText('Unfollow') : setText('Follow');
+        }
+        console.log(followers);
+    }
+
+    function postAction(){
+        if(text === 'Follow'){
+            const request = axios.post(`https://mock-api.bootcamp.respondeai.com.br/api/v1/linkr/users/${id}/follow`,{}, userToken);
+            request.then(successCaseFollow).catch(errorCase);
+        }else{
+            const request = axios.post(`https://mock-api.bootcamp.respondeai.com.br/api/v1/linkr/users/${id}/unfollow`,{}, userToken);
+            request.then(successCaseUnfollow).catch(errorCase);
+        }
+    }
+
+    function successCaseFollow(){
+        alert('Seguiu');
+    }
+
+    function successCaseUnfollow(){
+        alert('Deixou de seguir');
+    }
+
+    function errorCase(){
+        alert('Erro');
+    }
+
     return(
-        <Button>Follow</Button>
+        <Button onClick={postAction}>{text}</Button>
     )
 }
 
